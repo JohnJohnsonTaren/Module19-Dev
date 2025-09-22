a0d5d2162691b6eb0aaaa4916768b41332c0dfe7
JohnJohnsonTaren <61081845+JohnJohnsonTaren@users.noreply.github.com>
2025-09-22 21:58:30 +0300

    Delete build directory

--- build/resources/main/db/migration/V4__Add_Yaroslav_Lastivka_user.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (username, password, authority)
-VALUES ('Yaroslav Lastivka', '$2a$10$suxTZ1dWo8r69pMhDCXh4Ot6XdklRm3Ez5GDvaMXsPnCCy1D5yjxm', 'ROLE_USER');
--- build/resources/main/db/migration/V5__Add_Tytarenko_Denis.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (username, password, authority)
-VALUES ('Tytarenko Denis', '$2a$10$slBA5bSVTgcEPzDrdA1QyeFQQraISLZa2S7J9btVnRHAlrIhTMhCG', 'ROLE_USER');
