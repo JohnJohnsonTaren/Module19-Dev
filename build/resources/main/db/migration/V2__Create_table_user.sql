b1d235befe550a0a46f7112b603894e3e7142396
JohnJohnsonTaren <tytar.den@gmail.com>
2025-09-22 21:58:07 +0300

    https://github.com/JohnJohnsonTaren/Module19-Dev.git

---
+++ build/resources/main/db/migration/V2__Create_table_user.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(200) NOT NULL,
+    authority VARCHAR(100) NOT NULL
+);
---
+++ build/resources/main/db/migration/V3__Add_test_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, password, authority)
+VALUES ('user', '$2a$10$my47azfMiyyOxGcQaJWfX./lN8NzcQ6PDk..t673A7.jVHhgeD2ey', 'ROLE_USER');
